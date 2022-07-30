d9bda20eaf74fb899b97ebe95a9dbe4817183f15
Daniel Kertesz <daniel@spatof.org>
2022-07-30 16:54:45 +0100

    bluetooth stuff

--- v2/bluetooth.go
+++ v2/bluetooth.go
@@ -6,6 +6,97 @@ import (
 	"tinygo.org/x/bluetooth"
 )
 
+func findDevice(adapter *bluetooth.Adapter, name string) (bluetooth.Addresser, error) {
+	var result bluetooth.Addresser
+
+	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+		if device.LocalName() == "GB03" {
+			if err := adapter.StopScan(); err != nil {
+				fmt.Printf("error stopping scan: %s\n", err)
+				return
+			}
+			result = device.Address
+		}
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func sendCommands(queue [][]byte) error {
+	var adapter = bluetooth.DefaultAdapter
+	if err := adapter.Enable(); err != nil {
+		return err
+	}
+
+	uuid1, err := bluetooth.ParseUUID("0000ae30-0000-1000-8000-00805f9b34fb")
+	if err != nil {
+		return err
+	}
+
+	uuid2, err := bluetooth.ParseUUID("0000af30-0000-1000-8000-00805f9b34fb")
+	if err != nil {
+		return err
+	}
+
+	// TX_CHARACTERISTIC_UUID = '0000ae01-0000-1000-8000-00805f9b34fb'
+	uuid3, err := bluetooth.ParseUUID("0000ae01-0000-1000-8000-00805f9b34fb")
+	if err != nil {
+		return err
+	}
+
+	deviceAddr, err := findDevice(adapter, "GB03")
+	if err != nil {
+		return err
+	}
+
+	device, err := adapter.Connect(deviceAddr, bluetooth.ConnectionParams{})
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := device.Disconnect(); err != nil {
+			fmt.Printf("error disconnecting: %s\n", err)
+		}
+	}()
+
+	services, err := device.DiscoverServices([]bluetooth.UUID{uuid1, uuid2})
+	if err != nil {
+		return err
+	}
+
+	for _, service := range services {
+		fmt.Printf("service: %v\n", service)
+		chs, err := service.DiscoverCharacteristics([]bluetooth.UUID{uuid3})
+		if err != nil {
+			return err
+		}
+
+		tx := chs[0]
+		for _, cmd := range queue {
+			sendbuf := cmd
+
+			for len(sendbuf) != 0 {
+				partlen := 20
+				if len(sendbuf) < 20 {
+					partlen = len(sendbuf)
+				}
+
+				part := sendbuf[:partlen]
+				sendbuf = sendbuf[partlen:]
+				if _, err := tx.WriteWithoutResponse(part); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
 func sendPayload(payload []byte) error {
 	var adapter = bluetooth.DefaultAdapter
 	if err := adapter.Enable(); err != nil {
